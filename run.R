01348216b55a813c2cbfbebc28e801617d008781
rhijmans <r.hijmans@gmail.com>
2023-11-05 13:57:55 +0100

    rename

--- run.R
+++ run.R
@@ -1,13 +1,14 @@
 RAB_potato_1_ONA-data.R
+RAB_potato_11_predict-quefts_plots - Copy.R
 RAB_potato_2_field-data.R
 RAB_potato_3_exploratory-plots.R
-RAB_potato_4_compute_blup.R
-RAB_potato_5_blup-plots.R
+RAB_potato_4_compute-blup.R
+RAB_potato_5_compute-blup_plots.R
 RAB_potato_6_reverse-quefts.R
 RAB_potato_6_reverse-quefts_plots.R
-RAB_potato_7_predict_soil-supply.R
-RAB_potato_7_predict_soil-supply_plots.R
-RAB_potato_8_predict_QFTS.R
-RAB_potato_8_predict_QFTS_plots.R
-RAB_potato_9_predict_RF.R
-RAB_potato_9_predict_RF_plots.R
+RAB_potato_7_predict-soil-supply.R
+RAB_potato_7_predict-soil-supply_plots.R
+RAB_potato_8_predict-quefts.R
+RAB_potato_8_predict-quefts_plots.R
+RAB_potato_9_predict-RF.R
+RAB_potato_9_predict-RF_plots.R
