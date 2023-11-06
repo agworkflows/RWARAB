3eddde34f9d731a9a1c98845e7704d79484ca1b5
rhijmans <r.hijmans@gmail.com>
2023-11-06 05:58:40 +0100

    r

--- run.R
+++ run.R
@@ -1,14 +1,13 @@
 RAB_potato_1_ONA-data.R
-RAB_potato_11_predict-quefts_plots - Copy.R
+RAB_potato_10_predict-soil-supply_plots.R
+RAB_potato_11_predict-RF.R
+RAB_potato_12_predict-RF_plots.R
 RAB_potato_2_field-data.R
 RAB_potato_3_exploratory-plots.R
 RAB_potato_4_compute-blup.R
 RAB_potato_5_compute-blup_plots.R
 RAB_potato_6_reverse-quefts.R
-RAB_potato_6_reverse-quefts_plots.R
-RAB_potato_7_predict-soil-supply.R
-RAB_potato_7_predict-soil-supply_plots.R
+RAB_potato_7_reverse-quefts_plots.R
 RAB_potato_8_predict-quefts.R
 RAB_potato_8_predict-quefts_plots.R
-RAB_potato_9_predict-RF.R
-RAB_potato_9_predict-RF_plots.R
+RAB_potato_9_predict-soil-supply.R
