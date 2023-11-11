b69647d1a39ed70f0477a4b62d96ea270f6055e4
rhijmans <r.hijmans@gmail.com>
2023-11-11 13:16:21 +0100

    upo

--- setup.R
+++ setup.R
@@ -1,27 +1,25 @@
 ## each agvise workflow must have a script like this
 
-## add a rootdir for each computer that is used
-if (system('hostname', TRUE) == "LAPTOP-IVSPBGCA") { 
-	rootdir <- "d:/agwise"
-	geodata_dir <- rootdir
-} else if (system('hostname', TRUE) == "XYZ") { 
-	rootdir <- "c:/workspace/agwise"
-	geodata_dir <- rootdir
-} else { 
-	rootdir <- "."
-	geodata_dir <- "."
+initialize <- function(...) {
+
+	## add a rootdir for each computer that is used
+	if (system('hostname', TRUE) == "LAPTOP-IVSPBGCA") { 
+		rootdir <- "d:/agwise"
+	} else if (system('hostname', TRUE) == "XYZ") { 
+		rootdir <- "c:/workspace/agwise"
+	} else { 
+		rootdir <- "."
+	}
+
+	project <- "RWA_potato"
+
+	#must have this line 
+	prj_path <- file.path(rootdir, project)
+	dir.create(prj_path, FALSE, TRUE)
+
+	#make sure required packages are installed
+	wow::install_pkgs(c("geodata", "ranger", "randomForest", "lme4", "dplyr"))
+	wow::install_pkgs("Rquefts", update=TRUE)
+
+	prj_path 
 }
-
-project <- "RWA_potato"
-
-#must have this line 
-prj_path <- file.path(rootdir, project)
-
-#make sure required packages are installed
-wow::install_pkgs(c("geodata", "ranger", "randomForest", "lme4", "dplyr"))
-wow::install_pkgs("Rquefts", update=TRUE)
-
-#set the default geodata path
-geodata::geodata_path(geodata_dir)
-
-
